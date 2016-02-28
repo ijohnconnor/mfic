37631a112b923b6fd4f2f11387b8d6cfc79c4bff
John <shubindmitry@yandex.ru>
2016-02-28 22:08:41 +0400

    удаляем

--- simulate/do_lib.do
+++
@@ -1,6 +0,0 @@
-
-vlib work
-             
-vlog  ../altera_libs/220model.v                                                  
-vlog  ../altera_libs/altera_mf.v                         
-          
--- simulate/open_detect_target.m
+++
@@ -1,8 +0,0 @@
-clc; clear; close all;
-
-%% Импульсы обнаружения
-detection_impulse  =  dlmread('scen1\detection_impulse1.txt');
-
-
-%% Формирование импульсов цели
-target  =  define_target_impulse_dopler(160, 1, detection_impulse, 'draw');
